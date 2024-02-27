1bece35776d43c919e1e02e3f9c9c15935346cc3
djurovic <33664772+djurovic@users.noreply.github.com>
2024-02-27 23:00:47 +0100

    Moved controller to presentation layer, added AutoMapper and created Data transfer objects

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -13,10 +13,13 @@ builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.AddAutoMapper(typeof(Program));
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
+
 
 var app = builder.Build();
 
